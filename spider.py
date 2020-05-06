0b0083b50a9b2517832eb0a6913097f9825e680d
xjl <1348200269@qq.com>
2020-05-06 17:24:50 +0800

    update

--- spider.py
+++ spider.py
@@ -2,16 +2,16 @@ import json
 from requests import get
 
 def GetPic():
-    api_url = r'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
+    api_url = r'https://api.lolicon.app/setu/?apikey=962440885eb117e14f71d1'
     api = get(api_url)
     json_data = json.loads(api.text)
-    pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
-    start_date = json_data['images'][0]['startdate']
-    open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
+    pic_url = json_data['data'][0]['url']
+    pid = json_data['data'][0]['pid']
+    open(r'./json/{0}.json'.format(pid), 'wb').write(api.content)
     print('Create Json Success!')
     pic = get(pic_url, stream=True)
     if(pic.status_code == 200):
-        open(r'./pic/{0}.png'.format(start_date), 'wb').write(pic.content)
+        open(r'./pic/{0}.jpg'.format(pid), 'wb').write(pic.content)
         print('Create Image Success!')
     else:
         print('Create Image Faild!')
