8191d1f33e819bb577e4494a27d0d24b1b1dc861
XJLZ <1348200269qqxjl@gmail.com>
2020-05-07 08:57:55 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -8,14 +8,14 @@ def GetPic():
     pic_url = json_data['data'][0]['url']
     pid = json_data['data'][0]['pid']
     open(r'./json/{0}.json'.format(pid), 'wb').write(api.content)
-    open(r'./json/{0}.txt'.format(pid), 'wb').write(pid)
     print('Create Json Success!')
-    # pic = get(pic_url, stream=True)
-    # if(pic.status_code == 200):
-    #     open(r'./pic/{0}.jpg'.format(pid), 'wb').write(pic.content)
-    #     print('Create Image Success!')
-    # else:
-    #     print('Create Image Faild!')
+    url = 'https://i.pixiv.cat/img-original/img/2019/01/16/16/25/23/72683488_p0.jpg'
+    pic = get(url, stream=True)
+    if(pic.status_code == 200):
+        open(r'./pic/{0}.jpg'.format(pid), 'wb').write(pic.content)
+        print('Create Image Success!')
+    else:
+        print('Create Image Faild!')
 
 if __name__ == "__main__":
     GetPic()
